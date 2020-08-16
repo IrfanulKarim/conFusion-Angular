d12ecb704b599db0bd5b281870d72ca7e2b6b1e8
Irfanul Karim <karimirfanul10@gmail.com>
2020-08-16 07:11:26 +0600

    Components Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import { FlexLayoutModule} from '@angular/flex-layout';
+import { MatListModule} from '@angular/material/list';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button'; 
 
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { MatListModule} from '@angular/material/list';
+import { from } from 'rxjs';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +25,10 @@ import { MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
