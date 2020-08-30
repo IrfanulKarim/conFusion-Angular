c919d7a95b93eb63a87fdafe5ba6c5b97dc7c92b
Irfanul Karim <karimirfanul10@gmail.com>
2020-08-30 17:27:39 +0600

    Single Page Application Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -13,4 +13,12 @@ export class DishService {
   getDishes(): Dish[] {
     return DISHES;
   }
+
+  getDish(id: string): Dish{
+    return DISHES.filter((dish)=>(dish.id===id))[0];
+  }
+
+  getFeatureDish(): Dish{
+    return DISHES.filter((dish)=> dish.featured)[0];
+  }
 }
